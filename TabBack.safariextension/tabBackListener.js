3d968b0c76373b26862704dc11d2aa87a800e3a8
rageandqq <chitley.sameer@gmail.com>
2014-11-22 22:09:01 -0500

    Fixed indenting.

--- TabBack.safariextension/tabBackListener.js
+++ TabBack.safariextension/tabBackListener.js
@@ -3,18 +3,18 @@ var keys = [];
 var flag = false;
 
 function dHandler(e) {
-    keys[e.keyCode] = true;
-    if (e.keyCode == 84 && e.altKey && e.shiftKey && !flag) { 
-      flag = !flag;
-      safari.self.tab.dispatchMessage('TabBack-down');
-    }
+  keys[e.keyCode] = true;
+  if (e.keyCode == 84 && e.altKey && e.shiftKey && !flag) { 
+    flag = !flag;
+    safari.self.tab.dispatchMessage('TabBack-down');
+  }
 }
 
 function uHandler(e) {
-    keys[e.keyCode] = false;
-    if ((!keys[84] || (!keys[18] && !keys[16]) )&& flag) {
-      flag = !flag;
-    }
+  keys[e.keyCode] = false;
+  if ((!keys[84] || (!keys[18] && !keys[16]) )&& flag) {
+    flag = !flag;
+  }
 }
 
 window.addEventListener('keydown', dHandler, false);
