e608f296b9e980edbdad07adb39fccbd9e073529
rageandqq <chitley.sameer@gmail.com>
2014-11-22 21:48:02 -0500

    Finished listener, changed to Alt+Shift+T due to Cmd+Shift+T being reserved by Safari

---
+++ TabBack.safariextension/tabBackListener.js
@@ -0,0 +1,20 @@
+var keys = [];
+var flag = false;
+
+function dHandler(e) {
+    keys[e.keyCode] = true;
+    if (e.keyCode == 84 && e.altKey && e.shiftKey && !flag) { 
+      flag = !flag;
+      safari.self.tab.dispatchMessage('TabBack-down');
+    }
+}
+
+function uHandler(e) {
+    keys[e.keyCode] = false;
+    if ((!keys[84] || (!keys[18] && !keys[16]) )&& flag) {
+      flag = !flag;
+    }
+}
+
+window.addEventListener('keydown', dHandler, false);
+window.addEventListener('keyup', uHandler, false);
